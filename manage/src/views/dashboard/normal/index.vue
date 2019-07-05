f0f6958b31ba4aa71937639c5e70e9dedcb24f64
cn-wx <xuwang2@student.unimelb.edu.au>
2019-07-05 23:41:31 +1000

    [UPDATE] normal dashboard

---
+++ manage/src/views/dashboard/normal/index.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="dashboard-editor-container">
+    <div class=" clearfix">
+      <div class="info-container">
+        <span class="display_name">欢迎您，{{ name }}</span>
+        <div class="dashboard-text">管理员权限: <span v-for="role in roles" :key="role">{{ role }}</span></div>
+      </div>
+    </div>
+    <div>
+      <img src="@/assets/avatar/suave_logo.jpeg" class="emptyGif">
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  name: 'DashboardNormal',
+  data() {
+    return {
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'name',
+      'avatar',
+      'roles'
+    ])
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .emptyGif {
+    display: block;
+    width: 45%;
+    margin: 0 auto;
+  }
+  .dashboard-editor-container {
+    background-color: #e3e3e3;
+    min-height: 100vh;
+    padding: 50px 60px 0px;
+    .pan-info-roles {
+      font-size: 12px;
+      font-weight: 700;
+      color: #333;
+      display: block;
+    }
+    .info-container {
+      position: relative;
+      margin-left: 190px;
+      height: 150px;
+      line-height: 200px;
+      .display_name {
+        font-size: 48px;
+        line-height: 48px;
+        color: #212121;
+        position: absolute;
+        top: 25px;
+      }
+    }
+  }
+</style>
