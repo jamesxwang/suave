92a432c0a0b762a55adc634e4af3b76021fca187
cn-wx <xuwang2@student.unimelb.edu.au>
2019-07-05 23:21:16 +1000

    [ADD] super dashboard

--- manage/src/views/dashboard/index.vue
+++ manage/src/views/dashboard/index.vue
@@ -1,20 +1,34 @@
 <template>
   <div class="dashboard-container">
-    <div class="dashboard-text">Welcome, {{ name }}!</div>
-    <div class="dashboard-text">管理员权限: <span v-for="role in roles" :key="role">{{ role }}</span></div>
+    <!-- <div class="dashboard-text">Welcome, {{ name }}!</div>
+    <div class="dashboard-text">管理员权限: <span v-for="role in roles" :key="role">{{ role }}</span></div> -->
+    <component :is="currentRole" />
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+import superDashboard from './super'
+import normalDashboard from './normal'
 
 export default {
   name: 'Dashboard',
+  components: { superDashboard, normalDashboard },
+  data() {
+    return {
+      currentRole: 'superDashboard'
+    }
+  },
   computed: {
     ...mapGetters([
       'name',
       'roles'
     ])
+  },
+  created() {
+    if (!this.roles.includes('super')) {
+      this.currentRole = 'normalDashboard'
+    }
   }
 }
 </script>
