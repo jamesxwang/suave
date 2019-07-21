12a49f27aa13b8e2ceaa05c8f1d0a637756a8ae5
cn-wx <xuwang2@student.unimelb.edu.au>
2019-07-22 00:00:37 +1000

    [UPDATE] contact router

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -13,18 +13,19 @@
                 <i class="iconfont icon-view"></i>
                 <p>查看订单</p>
             </router-link>
-            <router-link class="flex-1" to="/register" tag="li">
+            <!-- <router-link class="flex-1" to="/register" tag="li">
                 <i class="iconfont icon-service"></i>
                 <p>联系客服</p>
-            </router-link>
-            <!-- <li class="flex-1">
+            </router-link> -->
+            <li class="flex-1" @click="returnToWechat">
                 <i class="iconfont icon-service"></i>
                 <p>联系客服</p>
-            </li> -->
+            </li>
 		</ul>
 	</div>
 </template>
 <script>
+let wx = require('weixin-js-sdk')
 export default {
     name: 'app',
     data () {
@@ -37,6 +38,14 @@ export default {
     methods: {
     	randomOrder () {
             this.$emit('placeOrder')
+        },
+        returnToWechat () {
+            this.$toast('宝宝可以直接向公众号发消息联系客服~')
+            setTimeout(() => {
+                window.opener = null
+                window.close()
+                wx.closeWindow();
+            }, 3 * 1000);
         }
     },
     created () {
