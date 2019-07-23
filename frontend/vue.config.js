3485cfb479c6aafaa32906f9ff1160c59cb4b426
James Wang <36892657+cn-wx@users.noreply.github.com>
2019-07-23 12:57:04 +1000

    Update vue.config.js

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -5,14 +5,14 @@ module.exports = {
   },
   // chainWebpack: config => {},
   productionSourceMap: false,
-  publicPath: process.env.NODE_ENV === 'production' ? 'http://47.74.66.156/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? 'http://www.suavechat.com/' : '/',
   devServer: { 
     disableHostCheck: true,
     host: '0.0.0.0',
     port: 8080,
     proxy: {
       '/api': {
-        target: 'http://47.74.66.156:8081',
+        target: 'http://www.suavechat.com:8081',
         ws: true,
         changeOrigin: true
       }
